13da9a66d68a214c83f79c74d4c9a90c843d6e6d
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-12-14 13:38:55 +0000

    first post

---
+++ 322_FirstPost_Gombos.py
@@ -0,0 +1,11 @@
+from Post import Post
+
+all_posts_archive = []
+
+#your code here
+post1 = Post("Marie", "This will be my first post!")
+post2 = Post("Jack", "This will be my first post!")
+post3 = Post("Ash", "This will be my first post!")
+print(post1)
+print(post2)
+print(post3)
