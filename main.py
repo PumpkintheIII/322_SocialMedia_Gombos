d184b6c85a282f1b1bbe267c52243fda1a05f50c
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-12-14 13:24:56 +0000

    created 322_FirstPost_LastName.py and post.py

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ PLTW CSE 3.2.2 Social Media: Posting Objects
 Max Gombos
 """
 #This file is to allow me to specify the other Python files that run, Not the actual assignment!
+exec(open("322_FirstPost_Gombos.py").read())
